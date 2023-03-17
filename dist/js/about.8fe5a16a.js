f7503e89a57c95602f77e12f87f4e2760a2e8985
hector237 <85283152+hector237@users.noreply.github.com>
2023-03-16 23:59:18 -0400

    express server created

---
+++ dist/js/about.8fe5a16a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkinghood5"]=self["webpackChunkkinghood5"]||[]).push([[443],{1272:function(n,t,u){u.r(t),u.d(t,{default:function(){return r}});var s=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],i=u(1001),a={},l=(0,i.Z)(a,s,e,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.8fe5a16a.js.map
